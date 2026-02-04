217c36a9ec02077c6d47bc37eed9461ceb93556c
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2026-02-04 00:29:01 -0300

    feat(database): gera e aplica migrations do banco de dados

---
+++ src/drizzle/migrations/0000_wide_rhino.sql
@@ -0,0 +1,52 @@
+CREATE TABLE "followers" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"follower_login" text NOT NULL,
+	"followed_login" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "likes" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_login" text NOT NULL,
+	"post_id" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "posts" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_login" text NOT NULL,
+	"post_id" integer,
+	"message" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "sessions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_login" text NOT NULL,
+	"token" text NOT NULL,
+	"ip" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "sessions_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"login" text NOT NULL,
+	"name" text NOT NULL,
+	"password_hash" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_login_unique" UNIQUE("login")
+);
+--> statement-breakpoint
+ALTER TABLE "followers" ADD CONSTRAINT "followers_follower_login_users_login_fk" FOREIGN KEY ("follower_login") REFERENCES "public"."users"("login") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "followers" ADD CONSTRAINT "followers_followed_login_users_login_fk" FOREIGN KEY ("followed_login") REFERENCES "public"."users"("login") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "likes" ADD CONSTRAINT "likes_user_login_users_login_fk" FOREIGN KEY ("user_login") REFERENCES "public"."users"("login") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "likes" ADD CONSTRAINT "likes_post_id_posts_id_fk" FOREIGN KEY ("post_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_login_users_login_fk" FOREIGN KEY ("user_login") REFERENCES "public"."users"("login") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_post_id_posts_id_fk" FOREIGN KEY ("post_id") REFERENCES "public"."posts"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_login_users_login_fk" FOREIGN KEY ("user_login") REFERENCES "public"."users"("login") ON DELETE cascade ON UPDATE no action;
